7c3aa72a379d1f7bbb71c9997eeda0d1b86915d6
Justin <rinkaandchan@outlook.com>
2022-01-28 17:45:18 +1100

    UI improvements

--- GameOfLife/LifeGame.cs
+++ GameOfLife/LifeGame.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using System.Globalization;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -18,10 +19,53 @@ namespace GameOfLife
         private const int TILE_SIZE = 16;
 
         private Vector2i size = new (110, 70);
-        private bool running;
-        private int tickRate = 5;
+
+        private int t;
+        private int ticks
+        {
+            get => t;
+            set
+            {
+                t = value;
+                if (tickText != null)
+                    tickText.Text = "Ticks: " + t;
+            }
+        }
+        
+        private bool r;
+        private bool running
+        {
+            get => r;
+            set
+            {
+                // TODO: this is broken
+                runningText.Text = "Running: " + (r ? "True" : "False");
+                runningText.Colour = r ? Color.Green : Color.Red;
+                r = value;
+            }
+        }
+
+        private float tr = 0.05f;
+        private float tickRate
+        {
+            get => tr;
+            set
+            {
+                tr = value;
+                rateText.Text = "tickRate: " + Math.Round(tr, 2);
+            }
+        }
         
         private Container inst;
+        private Container info;
+
+        private SpriteText renderText;
+        private SpriteText rateText;
+        private SpriteText tickText;
+        private SpriteText deltaText;
+        private SpriteText runningText;
+        
+        private SpriteText editPlayText;
 
         public LifeGame()
         {
@@ -54,63 +98,131 @@ namespace GameOfLife
             float TEXT_SCALE = 1;
             float SPACING_Y = 15;
             int WORD_SPACING = 8;
+
+            var font = fonts.GetResource(SpriteFont.SegoeSmall);
+
+            info = new Container()
+            {
+                Position = new Vector2(-20, -130),
+                Anchor = Anchor.BottomRight,
+                Origin = Anchor.BottomRight,
+                Items = new IDrawable[]
+                {
+                    new SpriteText("STATS", font)
+                    {
+                        Position = new Vector2(0, SPACING_Y * 0),
+                        TextAlign = Align.Right,
+                        Colour = Color.Red,
+                        CharScale = TEXT_SCALE,
+                        WordSpacing = WORD_SPACING,
+                    },
+                    renderText = new SpriteText("RenderFrequency: idk", font)
+                    {
+                        Position = new Vector2(0, SPACING_Y * 1),
+                        TextAlign = Align.Right,
+                        Colour = Color.CornflowerBlue,
+                        CharScale = TEXT_SCALE,
+                        WordSpacing = WORD_SPACING,
+                    },
+                    rateText = new SpriteText("tickRate: " + tickRate.ToString(CultureInfo.CurrentCulture), font)
+                    {
+                        Position = new Vector2(0, SPACING_Y * 2),
+                        TextAlign = Align.Right,
+                        Colour = Color.CornflowerBlue,
+                        CharScale = TEXT_SCALE,
+                        WordSpacing = WORD_SPACING,
+                    },
+                    deltaText = new SpriteText("deltaTime", font)
+                    {
+                        Position = new Vector2(0, SPACING_Y * 3),
+                        TextAlign = Align.Right,
+                        Colour = Color.CornflowerBlue,
+                        CharScale = TEXT_SCALE,
+                        WordSpacing = WORD_SPACING,
+                    },
+                    runningText = new SpriteText("Running: False", font)
+                    {
+                        Position = new Vector2(0, SPACING_Y * 4),
+                        TextAlign = Align.Right,
+                        Colour = Color.Red,
+                        CharScale = TEXT_SCALE,
+                        WordSpacing = WORD_SPACING,
+                    },
+                    tickText = new SpriteText("Ticks: 0", font)
+                    {
+                        Position = new Vector2(0, SPACING_Y * 5),
+                        TextAlign = Align.Right,
+                        Colour = Color.CornflowerBlue,
+                        CharScale = TEXT_SCALE,
+                        WordSpacing = WORD_SPACING,
+                    },
+                    new SpriteText("Powered by Yasai", font)
+                    {
+                        Position = new Vector2(0, SPACING_Y * 6),
+                        TextAlign = Align.Right,
+                        Colour = Color.SpringGreen,
+                        CharScale = TEXT_SCALE,
+                        WordSpacing = WORD_SPACING,
+                    },
+                }
+            };
             
-            inst = new Container()
+            inst = new Container
             {
                 Position = new Vector2(10),
                 Items = new IDrawable[]
                 {
                     // we need better containers NOW
-                    new SpriteText("CONTROLS:", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("CONTROLS:", font)
                     {
                         Position = new Vector2(0, SPACING_Y*0),
                         Colour = Color.White,
                         CharScale = TEXT_SCALE,
                         WordSpacing = WORD_SPACING,
                     },
-                    new SpriteText("Play/Pause: Space", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("Play/Pause: Space", font)
                     {
                         Position = new Vector2(0, SPACING_Y*1),
                         Colour = Color.White,
                         CharScale = TEXT_SCALE,
                         WordSpacing = WORD_SPACING,
                     },
-                    new SpriteText("Faster: K/L/Up/Right", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("Slower: K/L/Up/Right", font)
                     {
                         Position = new Vector2(0, SPACING_Y*2),
                         Colour = Color.White,
                         CharScale = TEXT_SCALE,
                         WordSpacing = WORD_SPACING,
                     },
-                    new SpriteText("Slower: J/H/Down/Left", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("Faster: J/H/Down/Left", font)
                     {
                         Position = new Vector2(0, SPACING_Y*3),
                         Colour = Color.White,
                         CharScale = TEXT_SCALE,
                         WordSpacing = WORD_SPACING,
                     },
-                    new SpriteText("Step through: Enter (also pauses the simulation)", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("Step through: Enter (also pauses the simulation)", font)
                     {
                         Position = new Vector2(0, SPACING_Y*4),
                         Colour = Color.White,
                         CharScale = TEXT_SCALE,
                         WordSpacing = WORD_SPACING,
                     },
-                    new SpriteText("Generate random: R", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("Generate random: R (this clears the simulation first)", font)
                     {
                         Position = new Vector2(0, SPACING_Y*5),
                         Colour = Color.White,
                         CharScale = TEXT_SCALE,
                         WordSpacing = WORD_SPACING,
                     },
-                    new SpriteText("Clear: E", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("Clear: E", font)
                     {
                         Position = new Vector2(0, SPACING_Y*6),
                         Colour = Color.White,
                         CharScale = TEXT_SCALE,
                         WordSpacing = WORD_SPACING,
                     },
-                    new SpriteText("Left click to place, Right click to remove", fonts.GetResource(SpriteFont.SegoeSmall))
+                    new SpriteText("Left click to place, Right click to remove", font)
                     {
                         Position = new Vector2(0, SPACING_Y*8),
                         Colour = Color.White,
@@ -121,19 +233,35 @@ namespace GameOfLife
             };
 
             Root.Add(inst);
+            Root.Add(info);
+            Root.Add(editPlayText = new SpriteText("Press space to pause the simulation first", fonts.GetResource(SpriteFont.Segoe))
+            {
+                Anchor = Anchor.Center,
+                Origin = Anchor.Center,
+                Colour = Color.Red,
+                TextAlign = Align.Center,
+            });
+            // TODO: setting this before load doesnt do anything
+            editPlayText.Visible = false;
         }
 
-        private int timer;
+        private double deltaTime;
         public override void Update(FrameEventArgs args)
         {
             base.Update(args);
-            timer++; // <- this sucks
-
-            if (tickRate <= 0)
-                return;
             
-            if (timer % tickRate == 0 && running)
+            deltaText.Text = "deltaTime: " + deltaTime;
+
+            if (tickRate <= 0 || !running)
+                return;
+
+            deltaTime += args.Time;
+
+            while (deltaTime > tickRate)
+            {
                 tick();
+                deltaTime -= tickRate;
+            }
         }
 
         /// <summary>
@@ -143,6 +271,7 @@ namespace GameOfLife
         {
             grid.Tick();
             updateCells();
+            ticks++;
         }
         
         /// <summary>
@@ -153,6 +282,8 @@ namespace GameOfLife
         /// <returns></returns>
         private Grid generate(bool empty = false, double probability = 0.4)
         {
+            ticks = 0;
+            
             var rand = new Random();
             bool[][] situation = new bool[size.Y][];
             for (int y = 0; y < size.Y; y++)
@@ -183,12 +314,21 @@ namespace GameOfLife
                 }
             }
         }
-        
+
+        protected override void MouseUp(MouseButtonEventArgs args)
+        {
+            base.MouseUp(args);
+            editPlayText.Visible = false;
+        }
+
         protected override void MouseDown(MouseButtonEventArgs args)
         {
             base.MouseDown(args);
             if (running)
+            {
+                editPlayText.Visible = true;
                 return;
+            }
 
             int s = TILE_SIZE + 2;
             int x = (int) Math.Floor(MousePosition.X / s);
@@ -212,6 +352,7 @@ namespace GameOfLife
                 // pause
                 case Keys.Space:
                     running = !running;
+                    inst.Visible = !inst.Visible;
                     break;
                 
                 // faster
@@ -219,7 +360,7 @@ namespace GameOfLife
                 case Keys.L:
                 case Keys.Up:
                 case Keys.Right:
-                    tickRate --;
+                    tickRate += 0.01f;
                     break;
                 
                 // slower
@@ -227,7 +368,8 @@ namespace GameOfLife
                 case Keys.H:
                 case Keys.Down:
                 case Keys.Left:
-                    tickRate ++;
+                    if (tickRate > 0.02)
+                        tickRate -= 0.01f;
                     break;
                 
                 // step through
